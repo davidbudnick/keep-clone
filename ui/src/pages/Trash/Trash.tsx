4154bad75aa48afbdd9cf9d13a4073d95fee1044
David Budnick <david@budnick.ca>
2024-02-07 18:55:31 -0600

    fixed trash page

--- ui/src/pages/Trash/Trash.tsx
+++ ui/src/pages/Trash/Trash.tsx
@@ -31,6 +31,18 @@ const Trash: React.FC = () => {
         );
     }
 
+    if (data?.notes?.length === 0) {
+        return (
+            <div className="ml-12 flex h-[calc(100vh-17rem)] items-center justify-center p-4">
+                <div>
+                    <FaTrashCan size={90} className="mx-auto text-gray-500" />
+                    <p className="mt-8 text-center text-2xl text-gray-600">No notes in trash</p>
+                </div>
+            </div>
+
+        )
+    }
+
     return (
         <div className="ml-10 mt-14 p-4">
             <div className="mb-4 mt-4 flex justify-center">
@@ -44,22 +56,11 @@ const Trash: React.FC = () => {
                     }} className="ml-4 cursor-pointer text-blue-500 hover:underline">Empty Trash</button>
                 }
             </div>
-
-            {data?.notes?.length === 0
-                ?
-                <div className="ml-12 flex h-[calc(100vh-17rem)] items-center justify-center p-4">
-                    <div>
-                        <FaTrashCan size={90} className="mx-auto text-gray-500" />
-                        <p className="mt-8 text-center text-2xl text-gray-600">No notes in trash</p>
-                    </div>
-                </div>
-                :
-                <div className="ml-6 flex flex-wrap">
-                    {data?.notes?.map((note) => (
-                        <DeletedCard key={note.id} note={note} />
-                    ))}
-                </div>
-            }
+            <div className="ml-6 flex flex-wrap">
+                {data?.notes?.map((note) => (
+                    <DeletedCard key={note.id} note={note} />
+                ))}
+            </div>
         </div >
     )
 }
