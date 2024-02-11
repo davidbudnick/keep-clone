45935d5fa3dcac3d400060f5c9c1963a8ed42ade
David Budnick <david@budnick.ca>
2024-02-10 22:33:03 -0600

    chunk vendor in bundle

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite"
+import { defineConfig, splitVendorChunkPlugin } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import path from "path";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), splitVendorChunkPlugin()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
