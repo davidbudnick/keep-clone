2e4c4c469f0445427069b1f2fb21e845de2d1b45
David Budnick <david@budnick.ca>
2024-02-11 17:47:35 -0600

    update secret for UI

--- tf/ecr/main.tf
+++ tf/ecr/main.tf
@@ -11,6 +11,20 @@ resource "aws_ecr_repository" "keep_ui_staging" {
   }
 }
 
+resource "github_actions_environment_secret" "aws_access_key_id" {
+  repository      = local.repository_name
+  environment     = "build"
+  secret_name     = "AWS_ECR_UI_REPO"
+  plaintext_value = aws_ecr_repository.keep_ui_staging.name
+}
+
+resource "github_actions_environment_secret" "keep_ui_ecr_image_name_staging" {
+  repository      = local.repository_name
+  environment     = "staging"
+  secret_name     = "ECS_IMAGE_NAME_UI"
+  plaintext_value = "${aws_ecr_repository.keep_ui_staging.repository_url}:latest"
+}
+
 resource "aws_ecr_repository" "keep_server_staging" {
   name                 = "keep_server_staging"
   image_tag_mutability = "MUTABLE"
@@ -29,16 +43,5 @@ locals {
 
 }
 
-resource "github_actions_environment_secret" "aws_access_key_id" {
-  repository      = local.repository_name
-  environment     = "build"
-  secret_name     = "AWS_ECR_UI_REPO"
-  plaintext_value = aws_ecr_repository.keep_server_staging.name
-}
 
-resource "github_actions_environment_secret" "keep_ui_ecr_image_name_staging" {
-  repository      = local.repository_name
-  environment     = "staging"
-  secret_name     = "ECS_IMAGE_NAME_UI"
-  plaintext_value = "${aws_ecr_repository.keep_ui_staging.repository_url}:latest"
-}
+
