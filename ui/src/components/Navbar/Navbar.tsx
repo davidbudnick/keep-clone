72a02d4594cdb6efd2260c0f29a013846eb97ed5
David Budnick <david@budnick.ca>
2024-03-02 21:45:06 -0600

    verify user state in navbar before loading

--- ui/src/components/Navbar/Navbar.tsx
+++ ui/src/components/Navbar/Navbar.tsx
@@ -27,7 +27,6 @@ import { UpdateTheme } from "@/lib/theme";
 import { DEFUALT_MOBILE_WIDTH } from "@/constants/mobile";
 import { useMediaQuery } from "react-responsive";
 import { MdNote } from "react-icons/md";
-import { User } from "@/graphql/generated/schema";
 
 
 const Navbar: React.FC = () => {
@@ -36,7 +35,6 @@ const Navbar: React.FC = () => {
     const { t } = useTranslation();
     const [theme, setTheme] = useState<string>(auth.user?.settings.theme || DARK);
     const [locale, setLocale] = useState<string>(auth.user?.settings.locale || "");
-    const [user, setUser] = useState<User | undefined>(auth.user);
     const [isSelectOpen, setIsSelectOpen] = useState(false);
     const isMobile = useMediaQuery({ maxWidth: DEFUALT_MOBILE_WIDTH });
 
@@ -45,9 +43,6 @@ const Navbar: React.FC = () => {
         setLocale(auth.user?.settings.locale || "");
     }, [auth.user?.settings.locale, auth.user?.settings.theme]);
 
-    useEffect(() => {
-        setUser(auth.user);
-    }, [auth.user]);
 
 
     const GetPageName = () => {
@@ -72,111 +67,113 @@ const Navbar: React.FC = () => {
                             </span>
                         </Link>
                     </div>
-                    <div className="flex items-center">
+                    {auth.user &&
                         <div className="flex items-center">
-                            <div className="mx-2">
-                                {auth.isAuthenticated && !auth.loading &&
-                                    <>
-                                        {isSelectOpen && isMobile && (
-                                            <div
-                                                style={{
-                                                    position: "fixed",
-                                                    top: 0,
-                                                    left: 0,
-                                                    width: "100%",
-                                                    height: "100%",
-                                                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                                                    zIndex: 50,
+                            <div className="flex items-center">
+                                <div className="mx-2">
+                                    {auth.isAuthenticated && !auth.loading &&
+                                        <>
+                                            {isSelectOpen && isMobile && (
+                                                <div
+                                                    style={{
+                                                        position: "fixed",
+                                                        top: 0,
+                                                        left: 0,
+                                                        width: "100%",
+                                                        height: "100%",
+                                                        backgroundColor: "rgba(0, 0, 0, 0.5)",
+                                                        zIndex: 50,
+                                                    }}
+                                                    onClick={() => setIsSelectOpen(false)}
+                                                />
+                                            )}
+                                            <Select
+                                                onValueChange={(e) => {
+                                                    auth.update({
+                                                        settings: {
+                                                            locale: e,
+                                                            theme: theme,
+                                                        },
+                                                    });
+                                                    i18n.changeLanguage(
+                                                        e || locales.en
+                                                    );
                                                 }}
-                                                onClick={() => setIsSelectOpen(false)}
-                                            />
-                                        )}
-                                        <Select
-                                            onValueChange={(e) => {
+                                                onOpenChange={setIsSelectOpen}
+                                            >
+                                                <SelectTrigger className="w-[70px]">
+                                                    <SelectValue placeholder={locale} />
+                                                </SelectTrigger>
+                                                <SelectContent className="w-[70px]" defaultValue={locale} defaultChecked>
+                                                    {Object.keys(locales).map((locale) => (
+                                                        <SelectItem key={locale} value={locale}>{locale}</SelectItem>
+                                                    ))}
+                                                </SelectContent>
+                                            </Select>
+                                        </>
+                                    }
+                                </div>
+                                <div className="mt-1 mx-2">
+                                    {auth.isAuthenticated && !auth.loading &&
+                                        <Switch
+                                            disabled={!auth.isAuthenticated}
+                                            key={theme}
+                                            checked={
+                                                theme === DARK
+                                            }
+                                            onClick={() => {
+                                                const newTheme = theme === DARK ? LIGHT : DARK;
+                                                setTheme(newTheme);
                                                 auth.update({
                                                     settings: {
-                                                        locale: e,
-                                                        theme: theme,
+                                                        locale: locale,
+                                                        theme: newTheme,
                                                     },
                                                 });
-                                                i18n.changeLanguage(
-                                                    e || locales.en
-                                                );
+                                                UpdateTheme(newTheme);
                                             }}
-                                            onOpenChange={setIsSelectOpen}
-                                        >
-                                            <SelectTrigger className="w-[70px]">
-                                                <SelectValue placeholder={locale} />
-                                            </SelectTrigger>
-                                            <SelectContent className="w-[70px]" defaultValue={locale} defaultChecked>
-                                                {Object.keys(locales).map((locale) => (
-                                                    <SelectItem key={locale} value={locale}>{locale}</SelectItem>
-                                                ))}
-                                            </SelectContent>
-                                        </Select>
-                                    </>
-                                }
-                            </div>
-                            <div className="mt-1 mx-2">
-                                {auth.isAuthenticated && !auth.loading &&
-                                    <Switch
-                                        disabled={!auth.isAuthenticated}
-                                        key={theme}
-                                        checked={
-                                            theme === DARK
-                                        }
-                                        onClick={() => {
-                                            const newTheme = theme === DARK ? LIGHT : DARK;
-                                            setTheme(newTheme);
-                                            auth.update({
-                                                settings: {
-                                                    locale: locale,
-                                                    theme: newTheme,
-                                                },
-                                            });
-                                            UpdateTheme(newTheme);
-                                        }}
-                                    />
-                                }
+                                        />
+                                    }
 
-                            </div>
-                            <div className='mx-4'>
-                                {
-                                    auth.isAuthenticated && !auth.loading ?
-                                        <Popover>
-                                            <PopoverTrigger asChild className="cursor-pointer">
-                                                <Avatar>
-                                                    <AvatarImage className='h-10 w-10' src={user?.picture} />
-                                                    <AvatarFallback className='h-10 w-10'>{`${user?.givenName?.charAt(0) || ""}${user?.familyName?.charAt(0) || ""}`}</AvatarFallback>
-                                                </Avatar>
-                                            </PopoverTrigger>
-                                            <PopoverContent className="z-10 mt-2 w-80 rounded-lg border border-gray-300 bg-white p-4 shadow-lg dark:border-gray-700 dark:bg-black">
-                                                <div className="grid gap-4">
-                                                    <div className="space-y-2 text-center">
-                                                        <p className="text-xs font-bold">{user?.email}</p>
-                                                        {user?.hd &&
-                                                            <p className="text-xs">{t("navbar.managed_by")} {user?.hd}</p>
-                                                        }
-                                                        <div className='flex justify-center'>
-                                                            <Avatar className='mt-3'>
-                                                                <AvatarImage className='h-20 w-20' src={user?.picture} />
-                                                                <AvatarFallback className='h-20 w-20 text-xl'>{`${user?.givenName?.charAt(0)}${user?.familyName?.charAt(0)}`}</AvatarFallback>
-                                                            </Avatar>
+                                </div>
+                                <div className='mx-4'>
+                                    {
+                                        auth.isAuthenticated && !auth.loading ?
+                                            <Popover>
+                                                <PopoverTrigger asChild className="cursor-pointer">
+                                                    <Avatar>
+                                                        <AvatarImage className='h-10 w-10' src={auth.user?.picture} />
+                                                        <AvatarFallback className='h-10 w-10'>{`${auth.user?.givenName?.charAt(0) || ""}${auth.user?.familyName?.charAt(0) || ""}`}</AvatarFallback>
+                                                    </Avatar>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="z-10 mt-2 w-80 rounded-lg border border-gray-300 bg-white p-4 shadow-lg dark:border-gray-700 dark:bg-black">
+                                                    <div className="grid gap-4">
+                                                        <div className="space-y-2 text-center">
+                                                            <p className="text-xs font-bold">{auth.user?.email}</p>
+                                                            {auth.user?.hd &&
+                                                                <p className="text-xs">{t("navbar.managed_by")} {auth.user?.hd}</p>
+                                                            }
+                                                            <div className='flex justify-center'>
+                                                                <Avatar className='mt-3'>
+                                                                    <AvatarImage className='h-20 w-20' src={auth.user?.picture} />
+                                                                    <AvatarFallback className='h-20 w-20 text-xl'>{`${auth.user?.givenName?.charAt(0)}${auth.user?.familyName?.charAt(0)}`}</AvatarFallback>
+                                                                </Avatar>
+                                                            </div>
+                                                            <div className="pb-2 pt-1 text-xl font-light">{t("navbar.hi")}, {auth.user?.givenName} {auth.user?.familyName}!</div>
+                                                            <Button onClick={auth.logout}>
+                                                                <LogOut className="mr-2 h-4 w-4" /> {t("navbar.logout")}
+                                                            </Button>
                                                         </div>
-                                                        <div className="pb-2 pt-1 text-xl font-light">{t("navbar.hi")}, {user?.givenName} {user?.familyName}!</div>
-                                                        <Button onClick={auth.logout}>
-                                                            <LogOut className="mr-2 h-4 w-4" /> {t("navbar.logout")}
-                                                        </Button>
                                                     </div>
-                                                </div>
-                                            </PopoverContent>
-                                        </Popover>
-                                        :
-                                        <GoogleLogin locale={locale || locales.en} onSuccess={auth.login} />
-                                }
+                                                </PopoverContent>
+                                            </Popover>
+                                            :
+                                            <GoogleLogin locale={locale || locales.en} onSuccess={auth.login} />
+                                    }
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    }
                 </div>
             </div>
         </nav>
